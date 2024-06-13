634e50190b1015189a76c67bc5e98c9ab372b456
Branislav Buna <46035047+codeHysteria28@users.noreply.github.com>
2024-06-13 17:33:08 +0100

    adding my first csharp project which is a simple console guessing game

---
+++ TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new();
+int randomNumber = random.Next(1,5);
+bool hasWon = true;
+
+do {
+    Console.Write("Enter your guess: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    if(input == randomNumber){
+        Console.WriteLine("You Won !");
+        hasWon = false;
+    }else {
+        Console.WriteLine("Ups, wrong guess :( Try again next time...");
+    }
+}while(hasWon);
